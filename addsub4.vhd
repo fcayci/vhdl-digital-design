98a9cc3f99657dca77806d77bea29a17056251ae
Furkan Cayci <furkanca02@gmail.com>
2018-10-12 12:30:51 +0300

    addsub fixes

--- addsub4.vhd
+++ addsub4.vhd
@@ -6,15 +6,14 @@ use ieee.std_logic_1164.all;
 entity addsub4 is
     -- a, b are 4-bit inputs of the addsub4
     -- m is the adder / subtractor selector input ('0' - adder, '1' - subtractor)
-    -- s is the 4-bit sum output,
+    -- sum is the 4-bit sum output,
     -- cout (carry-out) is the output of the addsub4
     -- v is the overflow detector output
     port (
-        a, b : in  std_logic_vector(3 downto 0);
-        m    : in  std_logic;
-        s    : out std_logic_vector(3 downto 0);
-        v    : out std_logic;
-        cout : out std_logic );
+        a, b    : in  std_logic_vector(3 downto 0);
+        m       : in  std_logic;
+        sum     : out std_logic_vector(3 downto 0);
+        v, cout : out std_logic );
 end addsub4;
 
 architecture rtl of addsub4 is
@@ -28,12 +27,15 @@ architecture rtl of addsub4 is
     signal bin : std_logic_vector(3 downto 0) := (others => '0');
 begin
     -- xor each b input with the value of m to use it as is or take 1's complement
-    bin <= b xor (m & m & m & m);
+    bin(0) <= b(0) xor m;
+    bin(1) <= b(1) xor m;
+    bin(2) <= b(2) xor m;
+    bin(3) <= b(3) xor m;
 
-    f0 : full_adder port map ( x => a(0), y => bin(0), cin => m,    s => s(0), cout => c(0) );
-    f1 : full_adder port map ( x => a(1), y => bin(1), cin => c(0), s => s(1), cout => c(1) );
-    f2 : full_adder port map ( x => a(2), y => bin(2), cin => c(1), s => s(2), cout => c(2) );
-    f3 : full_adder port map ( x => a(3), y => bin(3), cin => c(2), s => s(3), cout => c(3) );
+    f0 : full_adder port map ( x => a(0), y => bin(0), cin => m,    s => sum(0), cout => c(0) );
+    f1 : full_adder port map ( x => a(1), y => bin(1), cin => c(0), s => sum(1), cout => c(1) );
+    f2 : full_adder port map ( x => a(2), y => bin(2), cin => c(1), s => sum(2), cout => c(2) );
+    f3 : full_adder port map ( x => a(3), y => bin(3), cin => c(2), s => sum(3), cout => c(3) );
 
     cout <= c(3);
     -- calculate overflow
--- addsub4_tb.vhd
+++ addsub4_tb.vhd
@@ -9,27 +9,29 @@ end addsub4_tb;
 architecture behavioral of addsub4_tb is
     -- declare addsub4 circut as a component
     component addsub4 is
-        port ( a, b : in std_logic_vector(3 downto 0); m : in std_logic; s : out std_logic_vector(3 downto 0); v, cout : out std_logic );
+    port (
+        a, b    : in  std_logic_vector(3 downto 0);
+        m       : in  std_logic;
+        sum     : out std_logic_vector(3 downto 0);
+        v, cout : out std_logic );
     end component;
-    --  Specifies which entity is bound with the component
-    for a0: addsub4 use entity work.addsub4;
 
     signal a, b : std_logic_vector(3 downto 0);
     -- simple 1-bit vector trick to convert integer to std_logic
     signal m : std_logic_vector(0 downto 0);
-    signal s : std_logic_vector(3 downto 0);
+    signal sum : std_logic_vector(3 downto 0);
     signal v, cout : std_logic;
 begin
     --  instantiate addsub4 component.
-    a0: addsub4 port map (a => a, b => b, m => m(0), s => s, v => v, cout => cout);
+    a0: addsub4 port map (a => a, b => b, m => m(0), sum => sum, v => v, cout => cout);
 
     process
     begin
-        wait for 1 ns;
+        wait for 100 ns;
         for sel in 0 to 1 loop
             m <= std_logic_vector(to_unsigned(sel, 1));
-            for i in 0 to 16 loop
-                for j in 0 to 16 loop
+            for i in 0 to 15 loop
+                for j in 0 to 15 loop
                     a <= std_logic_vector(to_unsigned(i, 4));
                     b <= std_logic_vector(to_unsigned(j, 4));
                     wait for 1 ns;
